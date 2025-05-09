657757c37b8185e3241d61ae7a389a2137fb3e7c
samuelBright09 <samuelbrightsk@gmail.com>
2025-05-09 16:50:43 +0000

    Add metadata functionality

--- scripts/playlist.js
+++ scripts/playlist.js
@@ -1,15 +1,53 @@
 const songs = [
   {
     id: 1,
-    title: "Kaabo",
-    artist: "Dunsin Oyekan",
     songUrl: "./assets/audios/Kaabo-Dunsin-Oyekan.mp3",
+    category: "gospel"
   },
   {
     id: 2,
-    title: "You are my rest",
-    artist: "Dr Tumi",
     songUrl: "./assets/audios/Dr-Tumi-You-Are-My-Rest.mp3",
+    category: "gospel"
+  },
+  {
+    id: 3,
+    songUrl: "./assets/audios/Dunsin-Oyekan-The Great-Revivalist-(320).mp3",
+    category: "gospel"
+  },
+  {
+    id: 4,
+    songUrl: "./assets/audios/Dunsin-Oyekan-Because He Is,I-Am-(320).mp3",
+    category: "gospel"
+  },
+  {
+    id: 5,
+    songUrl: "./assets/audios/Dunsin Oyekan - Stand in the Gap (320).mp3",
+    category: "gospel"
+  },
+  {
+    id: 6,
+    songUrl: "./assets/audios/Dunsin Oyekan - Maranatha (320).mp3",
+    category: "gospel"
+  },
+  {
+    id: 7,
+    songUrl: "./assets/audios/Dunsin-Oyekan-Dunsin Oyekan - I Will Stay (320).mp3",
+    category: "gospel"
+  },
+  {
+    id: 8,
+    songUrl: "./assets/audios/Dunsin Oyekan - Finger of God (320)",
+    category: "gospel"
+  },
+  {
+    id: 7,
+    songUrl: "./assets/audios/Dunsin Oyekan - Crown of Souls (320)",
+    category: "gospel"
+  },
+  {
+    id: 8,
+    songUrl: "./assets/audios/CITY OF GOD - Dunsin Oyekan @dunsinoyekan @worship @cityofgod",
+    category: "gospel"
   },
 ];
 
